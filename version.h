18e291411d15806320b69606279e3024c39193be
Brad Robinson <contact@toptensoftware.com>
2019-12-20 15:50:34 +1100

    b1.1.7

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 
-#define VER_A		0
+#define VER_A		1
 #define VER_B		1
-#define VER_C		118
+#define VER_C		7
 #define VER_D		0
-#define COPYRIGHT_STRING "Copyright © 2019 Topten Software. All Rights Reserved\0"
+#define COPYRIGHT_STRING "Copyright © 2012-2019 Topten Software. All Rights Reserved\0"
 	
